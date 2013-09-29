5050f5e6762aa6678d64a4d06fa9d86e6b1b3c46
Benedikt Heine <me@bebehei.de>
2013-09-29 23:14:36 +0100

    loading navbar.js now dynamic

--- dorayaki-child-bebehei/header.php
+++ dorayaki-child-bebehei/header.php
@@ -29,7 +29,7 @@
 <script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
 <![endif]-->
 <?php wp_head(); ?>
-<script type="text/javascript" src="/wp-content/themes/elmastudio-themebundle/child_dorayaki/js/navbar_scroll.js"></script>
+<script type="text/javascript" src="<?php echo get_stylesheet_directory_uri(); ?>/js/navbar_scroll.js"></script>
 </head>
 
 <body <?php body_class(); ?>>
